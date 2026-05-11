77459df71f27a590303034bd4d0938424a9e3e7a
qusaiabdalgader568-wq <qusaiabdalgader568@gmail.com>
2026-05-11 21:57:54 +0200

    Update ram.js

--- plugins/info/ram.js
+++ plugins/info/ram.js
@@ -3,18 +3,18 @@ import os from 'os';
 const handler = async (m, { conn }) => {
   const txt = `
   ⊱⋅ ────────────────── ⋅⊰
-🪾╎ الـمسـتـخـدم: ${(process.memoryUsage().rss / 1024 / 1024).toFixed(1)}MB
-🌳╎ الـمتـبــقـي: ${(os.freemem() / 1024 / 1024).toFixed(1)}MB
+ʚɞ╎ الـمسـتـخـدم: ${(process.memoryUsage().rss / 1024 / 1024).toFixed(1)}MB
+ʚɞ╎ الـمتـبــقـي: ${(os.freemem() / 1024 / 1024).toFixed(1)}MB
   ⊱⋅ ────────────────── ⋅⊰
 `;
 
   await conn.msgUrl(m.chat, txt, {
-    img: "https://i.ibb.co/35wMG2T7/IMG-20260429-WA0042.jpg" ,
+    img: "https://i.ibb.co/zHbcL54J/15c390b7649080456a49fc027f6b1118.jpg" ,
     caption: txt,
     title: "𝐒𝐩𝐞𝐞𝐝 / 𝐓𝐞𝐬𝐭",
     body: "𝐓𝐞𝐬𝐭𝐢𝐧𝐠 𝐭𝐡𝐞 𝐛𝐨𝐭'𝐬 𝐬𝐩𝐞𝐞𝐝: 𝐈𝐬 𝐢𝐭 𝐟𝐚𝐬𝐭 𝐨𝐫 𝐧𝐨𝐭?",
     newsletter: {
-      name: '𝐈𝐍 𝐓𝐎𝐉𝐈',
+      name: '𝐈𝐍 𝐀𝐘𝐀𝐍𝐎 𝐉𝐈',
       jid: '120363425314431422@newsletter'
     },
     big: false,
