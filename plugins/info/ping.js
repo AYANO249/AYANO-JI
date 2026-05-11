82aa4d4a307e4b75d8344f87d0b8325124b98087
qusaiabdalgader568-wq <qusaiabdalgader568@gmail.com>
2026-05-11 22:02:53 +0200

    Update ping.js

--- plugins/info/ping.js
+++ plugins/info/ping.js
@@ -5,11 +5,11 @@ const handler = async (m, { conn }) => {
   const ping = Number(end - start) / 1e6;
   
   await conn.msgUrl(m.chat, `⚡ سرعة البوت: ${ping.toFixed(2)}ms`, {
-    img: "https://i.ibb.co/jvBRpKjS/1a8ec40ebc4a3a44d8f81d41a9ab93b2.jpg",
+    img: "https://i.ibb.co/ksw2TCDQ/bfe2f787dae49dd9efe465d5c1203ecc.jpg",
     title: "𝐒𝐩𝐞𝐞𝐝 / 𝐓𝐞𝐬𝐭",
     body: "𝐓𝐞𝐬𝐭𝐢𝐧𝐠 𝐭𝐡𝐞 𝐛𝐨𝐭'𝐬 𝐬𝐩𝐞𝐞𝐝: 𝐈𝐬 𝐢𝐭 𝐟𝐚𝐬𝐭 𝐨𝐫 𝐧𝐨𝐭?",
     newsletter: {
-      name: '𝐓𝐎𝐉𝐈',
+      name: '𝐈𝐍 𝐀𝐘𝐀𝐍𝐎 𝐉𝐈',
       jid: '120363425314431422@newsletter'
     },
     big: false
