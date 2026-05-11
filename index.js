946af4454fffec61f0e2ce5d4dddfbb5e8af7032
qusaiabdalgader568-wq <qusaiabdalgader568@gmail.com>
2026-05-11 20:17:08 +0200

    Update bot and channel names to AYANO JI

--- index.js
+++ index.js
@@ -10,13 +10,11 @@ const client = new Client({
   fromMe: false,
   owners: [
   // Owner 1
-    { name: "𝐓𝐎𝐉𝐈", lid: "247579682029763@lid", jid: "2250778788908@s.whatsapp.net" },
+    { name: "𝐀𝐘𝐀𝐍𝐎 𝐉𝐈", lid: "247579682029763@lid", jid: "2250778788908@s.whatsapp.net" },
   // Owner 2
     { name: "𝐃𝐄𝐕", lid: "221307316789354@lid", jid: "249906024672@s.whatsapp.net" },
   // Owner 3
-    { name: "𝐓𝐎𝐉𝐈", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" },
-  // Owner 4
-   { name: "عمورتي", jid: "201050079089@s.whatsapp.net", lid: "51664513925368@lid" }
+    { name: "𝐀𝐘𝐀𝐍𝐎 𝐉𝐈", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" }
   ],
   settings: { noWelcome: true },
   commandsPath: './plugins'
@@ -33,23 +31,22 @@ if (!global.db) {
 /* =========== Config ========== */
 const { config } = client;
 config.info = {
-  nameBot: "𝐓𝐎𝐉𝐈 〈", // تم التأكد من الاسم TOJI
-  nameChannel: "𝐓𝐎𝐉𝐈 𝐃𝐄𝐕",
-  idChannel: "120363425314431422@newsletter", // تم تحديث معرف القناة
+  nameBot: "𝐀𝐘𝐀𝐍𝐎 𝐉𝐈 〈", 
+  nameChannel: "𝐀𝐘𝐀𝐍𝐎 𝐉𝐈 𝐃𝐄𝐕",
+  idChannel: "120363425314431422@newsletter", 
   urls: {
-    // تم استبدال رابط github برابط قناتك لحذف اسم Pomni من المعاينة
     repo: "https://whatsapp.com/channel/0029VbD3UpkG3R3aq6V1DW2X", 
     api: "https://emam-api.web.id",
     channel: "https://whatsapp.com/channel/0029VbD3UpkG3R3aq6V1DW2X"
   },
   copyright: {
-    pack: 'ڤـ ـ 𝐓𝐎𝐉𝐈 ـ ـا',
-    author: '𝐓𝐎𝐉𝐈' // حذف أي إشارة لاسم QUSAY
+    pack: 'ڤـ ـ 𝐀𝐘𝐀𝐍𝐎 𝐉𝐈 ـ ـا',
+    author: '𝐀𝐘𝐀𝐍𝐎 𝐉𝐈' 
   },
   images: [
-    "https://i.ibb.co/TxpVwQGC/IMG-20260429-WA0041.jpg",
-    "https://i.ibb.co/6c3XHHWG/IMG-20260429-WA0047.jpg",
-    "https://i.ibb.co/YSFZ4BS/IMG-20260429-WA0049.jpg"
+    "https://i.ibb.co/tTZhHZc9/5979c43babaaecdd0546dee5bc1c32ff.jpg",
+    "https://i.ibb.co/B2rFmwm7/0e20c78c1ac2759547ff83ed0a8bbe66.jpg",
+    "https://i.ibb.co/xKY3B7Gq/6131d7a246c6ead5aaad1a19b30c822a.jpg"
   ]
 };
 
